4814695d3eb9993259de0793b3ea6120d3047bcd
Rodrigo Lisboa <rsilveiralisboa@gmail.com>
2024-08-27 10:09:46 -0300

    add filtro input

--- script.js
+++ script.js
@@ -22,6 +22,10 @@ console.log(container)
  descricao.classList.add("ativo")
 // texto.classList.remove("ativo")
 
+function filtrarEntrada(input){
+ input.value = input.value.replace(/[^a-z]/g, '');
+}
+
 
 function criptografar(){
   console.log("Criptografado")
@@ -32,13 +36,23 @@ function criptografar(){
 
 
 
-    var textoModificado = document.querySelector('#texto').value;
+    var texto = document.querySelector('#texto').value;
+    // resultado.textContent = texto;
+    console.log("Texto armazenado:", texto); 
 
+    if(texto.includes("a") || texto.includes("e") || texto.includes("i") || texto.includes("o") || texto.includes("u")){
 
-    resultado.textContent = textoModificado;
-    
+      var textoModificado = texto
+      .replace(/e/g, 'enter')
+      .replace(/i/g, 'imes')
+      .replace(/a/g, 'ai')
+      .replace(/o/g, 'ober')
+      .replace(/u/g, 'ufat')
 
-    console.log("Texto armazenado:", textoModificado); 
+      console.log("O texto original era:" + texto)
+      console.log("O texto modificado ficou:" +textoModificado)
+      resultado.textContent = textoModificado;
+    }
 }
 
 
@@ -56,8 +70,22 @@ function descriptografar(){
  
 
     resultado.textContent = texto;
-
     console.log("Texto armazenado:", texto); 
+
+   
+    if(texto.includes("a") || texto.includes("e") || texto.includes("i") || texto.includes("o") || texto.includes("u")){
+
+      var textoModificado = texto
+      .replace(/enter/g, 'e')
+      .replace(/imes/g, 'i')
+      .replace(/ai/g, 'a')
+      .replace(/ober/g, 'o')
+      .replace(/ufat/g, 'u')
+
+      console.log("O texto original era:" + texto)
+      console.log("O texto modificado ficou:" +textoModificado)
+      resultado.textContent = textoModificado;
+    }
 }
 
 botaoCriptografar.addEventListener("click", criptografar)
